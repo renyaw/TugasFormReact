c2a703dd3a3249b5e367e6a98c97e3e2ba60be87
Nora Renada <78416459+renyaw@users.noreply.github.com>
2022-11-21 05:15:24 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,61 +1,87 @@
 import React, { Component } from "react";
-import { Text, StyleSheet, View, TouchableOpacity, TextInput, borderColor, borderRadius } from "react-native";
+import { View, Text, TextInput, Button, StyleSheet } from "react-native";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      jurusan: "",
-      kelas: "",
-      lantai: "",
-      isLogin: false,
-    };
+    this.state = { no_ruang: "", lantai: "", jurusan: "" };
   }
 
-  login = () => {
-    this.setState({
-      isLogin: true
-    })
-  }
+  InsertRecord = () => {
+    var no_ruang = this.state.no_ruang;
+    var lantai = this.state.lantai;
+    var jurusan = this.state.jurusan;
+
+    if (no_ruang.length == 0 || lantai.length == 0 || jurusan.length == 0) {
+      alert("Semua data wajib diisi, tidak boleh kosong!");
+    } else {
+      var InsertAPIURL = "http://10.0.2.2:80/api/insert.php";
+
+      var headers = {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      };
+
+      var Data = {
+        no_ruang: no_ruang,
+        lantai: lantai,
+        jurusan: jurusan,
+      };
+
+      fetch(insertAPIURL, {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(Data),
+      })
+        .then((response) => response.json())
+        .then((response) => {
+          alert(response[0].message);
+        })
+        .catch((error) => {
+          alert("Error" + error);
+        });
+    }
+  };
+
   render() {
-    const { jurusan, kelas, lantai, isLogin } = this.state;
     return (
-      <View style={styles.container}>
+      <View style={styles.ViewStyle}>
         <Text style={styles.title}> From Using React Native Kelompok 5</Text>
         <View style={styles.garis}></View>
-        {/* Form Dimulai */}
-        <View style={styles.isi}>
-          <View>
-            <Text>Jurusan : </Text>
-            <TextInput style={styles.inputan} placeholder="Masukkan Jurusan" value={jurusan} onChangeText={(jurusan) => this.setState({ jurusan })} />
-          </View>
-
-          <View>
-            <Text>Nomor Kelas : </Text>
-            <TextInput style={styles.inputan} placeholder="Masukkan Nomor Kelas" value={kelas} onChangeText={(kelas) => this.setState({ kelas })} />
-          </View>
-
-          <View>
-            <Text>Lantai : </Text>
-            <TextInput style={styles.inputan} placeholder="Masukkan Nomor Lantai" value={lantai} onChangeText={(lantai) => this.setState({ lantai })} />
-          </View>
-
-          <TouchableOpacity style={styles.button} onPress={() => this.login()}>
-            <Text style={styles.butuntext}>Masukkan Data</Text>
-          </TouchableOpacity>
-
-          {isLogin && <Text style={styles.alret}>Data Berhasil Dimasukkan!</Text>}
+        <View>
+          <Text>Kode Ruang :</Text>
+          <TextInput placeholder={"Masukkan Kode Ruang"} style={styles.txtStyle} onChangeText={(no_ruang) => this.setState({ no_ruang })} />
         </View>
+        <View>
+          <Text>Lantai :</Text>
+          <TextInput placeholder={"Masukkan Nomor Lantai"} keyboardType="numeric" style={styles.txtStyle} onChangeText={(lantai) => this.setState({ lantai })} />
+        </View>
+
+        <View>
+          <Text>Jurusan :</Text>
+          <TextInput placeholder={"Masukkan Jurusan"} style={styles.txtStyle} onChangeText={(jurusan) => this.setState({ jurusan })} />
+        </View>
+
+        <Button title={"Masukkan Data"} onPress={this.InsertRecord} />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    padding: 40,
-    paddingTop: 60,
+  ViewStyle: {
+    flex: 1,
+    padding: 25,
+    marginTop: 40,
+  },
+
+  txtStyle: {
+    borderWidth: 1,
+    borderColor: "grey",
+    marginBottom: 20,
+    padding: 10,
+    marginTop: 10,
+    borderRadius: 3,
   },
   title: {
     fontSize: 14,
@@ -65,30 +91,6 @@ const styles = StyleSheet.create({
   garis: {
     borderBottomWidth: 1,
     marginTop: 10,
-  },
-  isi: {
-    paddingTop: 20,
-  },
-  inputan: {
-    borderWidth: 1,
-    padding: 6,
-    marginTop: 8,
-    borderColor: "grey",
-    borderRadius: 3,
     marginBottom: 20,
-  },
-  button: {
-    marginTop: 20,
-    backgroundColor: "#5f9ea0",
-    padding: 15,
-    borderRadius: 40,
-    marginBottom: 10,
-  },
-  butuntext: {
-    textAlign: "center",
-    color: "white",
-  },
-  alret: {
-    color: "#006400",
   },
 });
